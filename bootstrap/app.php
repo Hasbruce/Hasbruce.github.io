aee0700ab881bf3b5974518a513801c8282a6652
紫色寒雪 <oncwnuNW_P37zY3zQtwmF0Jbv5j8@git.weixin.qq.com>
2019-07-05 10:35:30 +0800

    中英翻译插件

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,6 +40,9 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
+$app->register(
+    ShaoZeMing\LaravelTranslate\TranslateServiceProvider::class
+);
 
 /*
 |--------------------------------------------------------------------------
